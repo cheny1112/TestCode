c553f47742fc84f00ed9d4c0c931d641f6639bf4
cheny1112 <sichen1250@163.com>
2020-01-03 17:30:24 +0900

    C语言练习

---
+++ C.CPP/Practicing/10_fractional_num_add.c
@@ -0,0 +1,54 @@
+/*
+ * @Description: 分数相加
+ * @Author: cheny
+ * @Date: 2020-01-03 17:17:16
+ * @LastEditor: cheny
+ * @LastEditTime : 2020-01-03 17:29:54
+ * @FilePath: \C_code\TestCode\C.CPP\Practicing\10_fractional_num_add.c
+ */
+
+#include <stdio.h>
+
+
+int get_LCM(int a, int b){
+    int temp;
+    int remender;
+    int A = a;
+    int B = b;
+    int ans;
+    if(a < b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    //?while(a % b) = while(a % b != 0)
+    while(a % b){//!思路是用两数之积除以最小公倍数
+        remender = a % b;
+        a = b;
+        b = remender;
+    }
+    ans = A * B / b;
+    //printf("最小公倍数为:%d", ans);
+
+    return ans;
+}
+/**
+ * @description: 给定两个分数，输出这两个分数的和，并用分数形式表示。
+ * @param {a-分数一的分子，b-分数一的分母，} 
+ * @return: 0
+ */
+void fractionalNumAdd(int a, int b, int c, int d){
+    int lcm = get_LCM(b, d);
+    //printf("%d", get_LCM(b, d));
+    int a1 = lcm / b;
+    int c1 = lcm / d;
+    int ans = a1*a + c1*c;
+    printf("%d/%d", ans, lcm);
+}
+
+int main(){
+    int a = 1, b = 3, c = 1, d = 9;
+    fractionalNumAdd(a, b, c, d);
+
+    return 0;
+}
---
+++ C.CPP/Practicing/9_printPrimeNumber.c
@@ -0,0 +1,24 @@
+/*
+ * @Description: 打印素数
+ * @Author: cheny
+ * @Date: 2020-01-03 16:41:57
+ * @LastEditor: cheny
+ * @LastEditTime : 2020-01-03 17:18:22
+ * @FilePath: \C_code\TestCode\C.CPP\Practicing\9_isPrimeNumber.c
+ */
+
+#include <stdio.h>
+
+void printPrimeNumber(int n){
+    int i = 2;
+    for(int j = 3; j <= n; ++j){
+        
+    }
+}
+
+int main(){
+    int n = 10;
+    isPrimeNumber(n);
+
+    return 0;
+}
