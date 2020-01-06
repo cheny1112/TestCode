4e1bde2d3ae5a7d36ac27c3f59069837ce702cec
cheny1112 <sichen1250@163.com>
2020-01-06 22:19:23 +0900

    杨辉三角（未完成

---
+++ C.CPP/Practicing/15_yanghui_Triangle.c
@@ -0,0 +1,32 @@
+/*
+ * @Description: 杨辉三角
+ * @Author: cheny
+ * @Date: 2020-01-06 21:37:20
+ * @LastEditor: cheny
+ * @LastEditTime : 2020-01-06 22:19:01
+ * @FilePath: \c_lang\C_code\TestCode\C.CPP\Practicing\15_yanghui_Triangle.c
+ */
+
+
+#include <stdio.h>
+//! n is the size of the triangle
+void yanghuiTriangle(int n){
+    if(n = 1){
+        printf("1");
+    }
+    if(n = 2){
+        printf("1 11");
+    }
+    if(n > 2){
+        for(int i = 2; i < n; i ++){
+            
+        }
+    }
+    
+}
+
+int main(){
+    yanghuiTriangle(4);
+
+    return 0;
+}
